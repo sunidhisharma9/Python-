dedeff3547bb852f2520663583cbd786cfbad467
SUNIDHI SHARMA <42183434+sunidhisharma9@users.noreply.github.com>
2018-10-03 19:40:56 +0530

    Create 5.py

---
+++ basic/5.py
@@ -0,0 +1,18 @@
+def succ(N):
+    '''
+    OBJECTIVE: To find the successor of a number.
+    INPUT PARAMETER:
+          N: a non-negative number whose successor is to be calculated.
+    OUTPUT:
+          Successor of non-negative number 'N'.
+    '''
+    #Approach: succ(N) = N+1
+
+    if N>=0:
+        return N+1
+    print("Error: ",N," < 0.")
+    return
+
+
+#assert N>=0
+
