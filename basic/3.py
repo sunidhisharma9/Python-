87f432ebc9ddd9c882f095d4747053c0a591a437
SUNIDHI SHARMA <42183434+sunidhisharma9@users.noreply.github.com>
2018-10-03 19:40:04 +0530

    Create 3.py

---
+++ basic/3.py
@@ -0,0 +1,25 @@
+def pyramid():
+    '''
+    Objective: To print pyramid of asterisks '*' comprising of 4 lines of output.
+
+    '''
+    #Approach: use print statement for each output line
+
+    print('   *   ')
+    print('  ***  ')
+    print(' ***** ')
+    print('*******')
+
+def main():
+
+    
+    '''
+    Objective: To print pyramid of asterisks '*' comprising of 4 lines of output.
+
+    '''
+    #Approach: using pyramid() function
+
+    pyramid()
+
+if __name__=='__main__':
+    main()
