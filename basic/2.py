c3700d08481847fab047e629c77d527a394b7f09
SUNIDHI SHARMA <42183434+sunidhisharma9@users.noreply.github.com>
2018-10-03 19:39:40 +0530

    Create 2.py

---
+++ basic/2.py
@@ -0,0 +1,19 @@
+def rtTriangle():
+    '''
+    Objective: To Print Triangle form astrisk it comprises 4 lines output
+    '''
+    # Approach: Use a print function to print
+    print('*')
+    print('**')
+    print('***')
+    print('****')
+
+def main():
+    '''
+    Objective: To print right angle triangle function to print 4 line right angle triangle
+    '''
+    # Approach: Use rtTriangle function
+    rtTriangle()
+
+if __name__=='__main__':
+    main()
