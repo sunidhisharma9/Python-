9a56e89302c6c9249f41329958b0284eefcadff6
SUNIDHI SHARMA <42183434+sunidhisharma9@users.noreply.github.com>
2018-10-22 22:35:45 +0530

    Create area.py

---
+++ basic/area.py
@@ -0,0 +1,11 @@
+def area(a,b):
+    if a>b:
+        print("AREA OF RECTANGLE IS ")
+        print(a*b)
+
+    elif a==b:
+        print("AREA OF SQUARE IS ")
+        print(a*a)
+    else:
+        print("AREA OF TRIANGLE IS")
+        print(0.5*a*b)
